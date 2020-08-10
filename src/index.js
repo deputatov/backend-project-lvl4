99d468dd746b3f6db468126f713f7059630e4253
Deputatov Nikita <deputatovn@yandex.ru>
2020-08-10 18:29:28 +0300

    add rollbar

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-// @ts-check
-
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Rollbar from 'rollbar';
+import fastifyExpress from 'fastify-express';
+import app from '../server/index.js';
+
+app.register(fastifyExpress);
+
+const rollbar = new Rollbar({
+  accessToken: '2d6040f98f2b461d8ea09405ecfbf315',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+});
+
+app.use(rollbar.errorHandler());

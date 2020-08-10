b723501d912bd48a27fcc822e5b1d4e360b050b1
Deputatov Nikita <deputatovn@yandex.ru>
2020-08-10 18:58:32 +0300

    comment out module

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Rollbar from 'rollbar';
-import fastifyExpress from 'fastify-express';
-import app from '../server/index.js';
+// import Rollbar from 'rollbar';
+// import fastifyExpress from 'fastify-express';
+// import app from '../server/index.js';
 
-app.register(fastifyExpress);
+// app.register(fastifyExpress);
 
-const rollbar = new Rollbar({
-  accessToken: '2d6040f98f2b461d8ea09405ecfbf315',
-  captureUncaught: true,
-  captureUnhandledRejections: true,
-});
+// const rollbar = new Rollbar({
+//   accessToken: '2d6040f98f2b461d8ea09405ecfbf315',
+//   captureUncaught: true,
+//   captureUnhandledRejections: true,
+// });
 
-app.use(rollbar.errorHandler());
+// app.use(rollbar.errorHandler());

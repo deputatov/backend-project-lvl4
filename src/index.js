ce502c4fa9b505dc9b3e234ab4d225c278b7f5b8
Deputatov Nikita <deputatovn@yandex.ru>
2020-08-11 13:38:06 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Rollbar from 'rollbar';
-import fastifyExpress from 'fastify-express';
+// import fastifyExpress from 'fastify-express';
 import app from '../server/index.js';
 
 export default () => {
-  app.register(fastifyExpress);
+  // app.register(fastifyExpress);
 
   const rollbar = new Rollbar({
     accessToken: '2d6040f98f2b461d8ea09405ecfbf315',

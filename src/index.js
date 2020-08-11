89ded41249b1f79ca39af53ad2dbd6c7220488c0
Deputatov Nikita <deputatovn@yandex.ru>
2020-08-11 16:29:08 +0300

    remove fastify-express

--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import Rollbar from 'rollbar';
-import fastifyExpress from 'fastify-express';
+import Rollbar from 'rollbar';
 import app from '../server/index.js';
 
-app.register(fastifyExpress);
+export default () => {
+  const rollbar = new Rollbar({
+    accessToken: '2d6040f98f2b461d8ea09405ecfbf315',
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+  });
 
-// export default () => {
-//   app.register(fastifyExpress);
-
-//   const rollbar = new Rollbar({
-//     accessToken: '2d6040f98f2b461d8ea09405ecfbf315',
-//     captureUncaught: true,
-//     captureUnhandledRejections: true,
-//   });
-
-//   rollbar.log('Hello world!');
-//   console.log('test');
-//   // app.use(rollbar.errorHandler());
-// };
+  rollbar.log('Hello world!');
+  app.use(rollbar.errorHandler());
+};

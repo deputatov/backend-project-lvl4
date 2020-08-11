e8f533bdbcc7f85bd86b4e6ed3e1a5d1949e1e1c
Deputatov Nikita <deputatovn@yandex.ru>
2020-08-11 14:09:57 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Rollbar from 'rollbar';
-// import fastifyExpress from 'fastify-express';
-// import app from '../server/index.js';
+// import Rollbar from 'rollbar';
+import fastifyExpress from 'fastify-express';
+import app from '../server/index.js';
 
-export default () => {
-  // app.register(fastifyExpress);
+app.register(fastifyExpress);
 
-  const rollbar = new Rollbar({
-    accessToken: '2d6040f98f2b461d8ea09405ecfbf315',
-    captureUncaught: true,
-    captureUnhandledRejections: true,
-  });
+// export default () => {
+//   app.register(fastifyExpress);
 
-  rollbar.log('Hello world!');
-  console.log('test');
-  // app.use(rollbar.errorHandler());
-};
+//   const rollbar = new Rollbar({
+//     accessToken: '2d6040f98f2b461d8ea09405ecfbf315',
+//     captureUncaught: true,
+//     captureUnhandledRejections: true,
+//   });
+
+//   rollbar.log('Hello world!');
+//   console.log('test');
+//   // app.use(rollbar.errorHandler());
+// };

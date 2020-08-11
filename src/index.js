a4b739d43b48a5bbe63c82b809a428c17e18e290
Deputatov Nikita <deputatovn@yandex.ru>
2020-08-11 19:47:58 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -2,13 +2,27 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Rollbar from 'rollbar';
 import app from '../server/index.js';
 
-export default () => {
-  const rollbar = new Rollbar({
-    accessToken: '2d6040f98f2b461d8ea09405ecfbf315',
-    captureUncaught: true,
-    captureUnhandledRejections: true,
-  });
+const rollbar = new Rollbar({
+  accessToken: '2d6040f98f2b461d8ea09405ecfbf315',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+});
 
-  rollbar.log('Hello world!');
-  app.use(rollbar.errorHandler());
-};
+// export default () => {
+//   const rollbar = new Rollbar({
+//     accessToken: '2d6040f98f2b461d8ea09405ecfbf315',
+//     captureUncaught: true,
+//     captureUnhandledRejections: true,
+//   });
+
+//   rollbar.log('Hello world!');
+//   app.use(rollbar.errorHandler());
+// };
+
+// app.use((req, res) => {
+//   res.end('middleware');
+// });
+
+app.addHook('onError', async (request, reply, error) => {
+  await rollbar.log(error);
+});

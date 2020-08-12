30607c1206e1759dfdf536fc250166550b739ce9
Deputatov Nikita <deputatovn@yandex.ru>
2020-08-12 16:30:54 +0300

    add autoprefixer

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+/* eslint global-require: 0 */
+/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
+module.exports = {
+  plugins: [
+    require('autoprefixer'),
+  ],
+};

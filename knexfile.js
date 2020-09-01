99713ed9dfb043b27e182fdbdfe1560a7e259cc0
Deputatov Nikita <deputatovn@yandex.ru>
2020-09-01 19:34:07 +0300

    add knexfile connection parameters

--- knexfile.js
+++ knexfile.js
@@ -29,15 +29,29 @@ module.exports = {
   //   },
   //   migrations,
   // },
+  // production: {
+  //   client: 'pg',
+  //   connection: {
+  //     connectionString: process.env.DATABASE_URL,
+  //     ssl: {
+  //       rejectUnauthorized: false,
+  //     },
+  //     sslmode: 'require',
+  //   },
+  //   migrations,
+  // },
   production: {
     client: 'pg',
     connection: {
-      connectionString: process.env.DATABASE_URL,
+      host: process.env.DATABASE_HOST,
+      port: process.env.DATABASE_PORT,
+      database: process.env.DATABASE_NAME,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      sslmode: 'require',
       ssl: {
         rejectUnauthorized: false,
       },
-      sslmode: 'require',
     },
-    migrations,
   },
 };

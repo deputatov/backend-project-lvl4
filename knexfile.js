3607b6f884b3ff9bf6ef2a7e2ad02a1078176f1d
Deputatov Nikita <deputatovn@yandex.ru>
2020-09-02 15:48:04 +0300

    add environment variables

--- knexfile.js
+++ knexfile.js
@@ -29,26 +29,10 @@ module.exports = {
   //   },
   //   migrations,
   // },
-  // production: {
-  //   client: 'pg',
-  //   connection: {
-  //     connectionString: process.env.DATABASE_URL,
-  //     ssl: {
-  //       rejectUnauthorized: false,
-  //     },
-  //     sslmode: 'require',
-  //   },
-  //   migrations,
-  // },
   production: {
     client: 'pg',
     connection: {
-      host: process.env.DATABASE_HOST,
-      port: process.env.DATABASE_PORT,
-      database: process.env.DATABASE_NAME,
-      user: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      sslmode: 'require',
+      connectionString: process.env.DATABASE_URL,
       ssl: {
         rejectUnauthorized: false,
       },

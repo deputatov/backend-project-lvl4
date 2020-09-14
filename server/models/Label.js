a91b167bc8a534eb8450e4c9bd54d2c6f8cee2a1
Deputatov Nikita <deputatovn@yandex.ru>
2020-09-14 17:13:08 +0300

    add relation

--- server/models/Label.js
+++ server/models/Label.js
@@ -18,4 +18,18 @@ export default class Label extends unique(Model) {
       },
     };
   }
+
+  static get relationMappings() {
+    const TaskLabel = require('./TaskLabel.js');
+    return {
+      tasks: {
+        relation: Model.HasManyRelation,
+        modelClass: TaskLabel,
+        join: {
+          from: 'labels.id',
+          to: 'tasks_labels.label_id',
+        },
+      },
+    };
+  }
 }

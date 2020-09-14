05a8ccf2513fab828ce2d179ea26a95a3b647d51
Deputatov Nikita <deputatovn@yandex.ru>
2020-09-14 16:12:33 +0300

    add TaskLabel model

--- server/models/TaskLabel.js
+++ server/models/TaskLabel.js
@@ -1,1 +1,20 @@
 import { Model } from 'objection';
+
+export default class TaskLabel extends Model {
+  static get tableName() {
+    return 'tasks_labels';
+  }
+
+  static get idColumn() {
+    return 'id';
+  }
+
+  // static get relationMappings() {
+  //   const Task = require('./Task.js');
+  //   return {
+  //     tasks: {
+  //       relation: Model.
+  //     }
+  //   }
+  // }
+}

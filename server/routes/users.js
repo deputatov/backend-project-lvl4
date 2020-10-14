689e7297eeadc1d4df43ec850da2c59b7abea124
Deputatov Nikita <deputatovn@yandex.ru>
2020-10-14 20:18:58 +0300

    refactor

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,31 +1,39 @@
 // @ts-check
 
 import i18next from 'i18next';
+import { ValidationError } from 'objection';
 
 export default (app) => {
   app
+
     .get('/users', { name: 'users' }, async (req, reply) => {
       const users = await app.objection.models.user.query();
       reply.render('users/index', { users });
       return reply;
     })
+
     .get('/users/new', { name: 'newUser' }, (req, reply) => {
       const user = {};
       reply.render('users/new', { user });
     })
+
     .post('/users', async (req, reply) => {
       try {
-        const user = await app.objection.models.user.fromJson(req.body.object);
-        await app.objection.models.user.query().insert(user);
+        await app.objection.models.user.query().insert(req.body.object);
         req.flash('info', i18next.t('flash.users.create.success'));
         reply.code(201).redirect(302, app.reverse('root'));
         return reply;
-      } catch ({ data }) {
-        req.flash('error', i18next.t('flash.users.create.error'));
-        reply.code(422).render('users/new', { user: req.body.object, errors: data });
+      } catch (err) {
+        if (err instanceof ValidationError) {
+          req.flash('error', i18next.t('flash.users.create.error'));
+          reply.code(err.statusCode).render('users/new', { user: req.body.object, errors: err.data });
+          return reply;
+        }
+        reply.code(err.statusCode).type('application/json').send(err.data);
         return reply;
       }
     })
+
     .get('/users/:id/edit', async (req, reply) => {
       if (!req.signedIn) {
         req.flash('error', i18next.t('flash.users.authorizationError'));
@@ -37,10 +45,10 @@ export default (app) => {
         reply.code(403).redirect(302, app.reverse('users'));
         return reply;
       }
-      const data = { user: req.currentUser.$toJson() };
-      reply.render('users/edit', data);
+      reply.render('users/edit', { user: { ...req.currentUser } });
       return reply;
     })
+
     .patch('/users/:id', async (req, reply) => {
       const { id } = req.params;
       const { object } = req.body;
@@ -56,6 +64,7 @@ export default (app) => {
         return reply;
       }
     })
+
     .delete('/users/:id', async (req, reply) => {
       if (!req.signedIn) {
         req.flash('error', i18next.t('flash.users.authorizationError'));

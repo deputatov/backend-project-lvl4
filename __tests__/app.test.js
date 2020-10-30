f3fdec19c544f99cc550e0019c6f20c66a3ce8bb
Deputatov Nikita <deputatovn@yandex.ru>
2020-10-30 15:52:59 +0300

    add tasks tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -68,6 +68,24 @@ const labelUpdateData = {
   name: faker.lorem.word(),
 };
 
+const task = {
+  name: faker.lorem.word(),
+  description: faker.lorem.word(),
+  creatorId: 1,
+  statusId: 1,
+  executorId: 1,
+  labels: [1, 2],
+};
+
+const taskUpdateData = {
+  name: faker.lorem.word(),
+  description: faker.lorem.word(),
+  creatorId: 1,
+  statusId: 2,
+  executorId: 2,
+  labels: [1],
+};
+
 describe('CRUD users', () => {
   let server;
   let cookie;
@@ -402,5 +420,186 @@ describe('CRUD labels', () => {
 
     const deletedLabels = await server.objection.models.label.query();
     expect(deletedLabels).toEqual([]);
+  });
+});
+
+describe('CRUD tasks', () => {
+  let server;
+  let cookie;
+
+  beforeAll(async () => {
+    server = await app();
+    await server.objection.knex.migrate.latest();
+  });
+
+  afterAll(async () => {
+    await server.close();
+  });
+
+  it('users#create', async () => {
+    const user1 = await server.inject({
+      method: 'POST',
+      url: server.reverse('users#create'),
+      payload: { object: { ...user } },
+    });
+    expect(user1.statusCode).toBe(302);
+
+    const user2 = await server.inject({
+      method: 'POST',
+      url: server.reverse('users#create'),
+      payload: { object: { ...userUpdateData } },
+    });
+    expect(user2.statusCode).toBe(302);
+  });
+
+  it('session#create', async () => {
+    const auth = await server.inject({
+      method: 'POST',
+      url: server.reverse('session#create'),
+      payload: { object: { email: user.email, password: user.password } },
+    });
+    const { headers } = auth;
+    cookie = headers['set-cookie'];
+    expect(auth.statusCode).toBe(302);
+  });
+
+  it('statuses#create', async () => {
+    const status1 = await server.inject({
+      method: 'POST',
+      url: server.reverse('statuses#create'),
+      headers: { cookie },
+      payload: { object: { ...status } },
+    });
+    expect(status1.statusCode).toBe(302);
+
+    const status2 = await server.inject({
+      method: 'POST',
+      url: server.reverse('statuses#create'),
+      headers: { cookie },
+      payload: { object: { ...statusUpdateData } },
+    });
+    expect(status2.statusCode).toBe(302);
+  });
+
+  it('labels#create', async () => {
+    const label1 = await server.inject({
+      method: 'POST',
+      url: server.reverse('labels#create'),
+      headers: { cookie },
+      payload: { object: { ...label } },
+    });
+    expect(label1.statusCode).toBe(302);
+
+    const label2 = await server.inject({
+      method: 'POST',
+      url: server.reverse('labels#create'),
+      headers: { cookie },
+      payload: { object: { ...labelUpdateData } },
+    });
+    expect(label2.statusCode).toBe(302);
+  });
+
+  it('tasks#index', async () => {
+    const res = await server.inject({
+      method: 'GET',
+      url: server.reverse('tasks#index'),
+      headers: { cookie },
+    });
+    expect(res.statusCode).toBe(200);
+  });
+
+  it('tasks#new', async () => {
+    const res = await server.inject({
+      method: 'GET',
+      url: server.reverse('tasks#new'),
+      headers: { cookie },
+    });
+    expect(res.statusCode).toBe(200);
+  });
+
+  it('tasks#create', async () => {
+    const res = await server.inject({
+      method: 'POST',
+      url: server.reverse('tasks#create'),
+      headers: { cookie },
+      payload: { object: { ...task } },
+    });
+    expect(res.statusCode).toBe(302);
+
+    const createdTask = await server.objection.models.task
+      .query()
+      .first()
+      .withGraphFetched('labels')
+      .modifyGraph('labels', (builder) => {
+        builder.select('labels.id');
+      });
+    expect(
+      {
+        ...createdTask,
+        labels: createdTask.labels.map(({ id }) => id),
+      },
+    ).toMatchObject(task);
+
+    const requiredFields = await server.inject({
+      method: 'POST',
+      url: server.reverse('tasks#create'),
+      headers: { cookie },
+      payload: { object: { ...task, name: '' } },
+    });
+    expect(requiredFields.statusCode).toBe(400);
+  });
+
+  it('tasks#show', async () => {
+    const res = await server.inject({
+      method: 'GET',
+      url: server.reverse('tasks#show', { id: 1 }),
+      headers: { cookie },
+    });
+    expect(res.statusCode).toBe(200);
+  });
+
+  it('tasks#edit', async () => {
+    const res = await server.inject({
+      method: 'GET',
+      url: server.reverse('tasks#edit', { id: 1 }),
+      headers: { cookie },
+    });
+    expect(res.statusCode).toBe(200);
+  });
+
+  it('tasks#update', async () => {
+    const res = await server.inject({
+      method: 'PATCH',
+      url: server.reverse('tasks#update', { id: 1 }),
+      headers: { cookie },
+      payload: { object: { ...taskUpdateData } },
+    });
+    expect(res.statusCode).toBe(302);
+
+    const updatedTask = await server.objection.models.task
+      .query()
+      .first()
+      .withGraphFetched('labels')
+      .modifyGraph('labels', (builder) => {
+        builder.select('labels.id');
+      });
+    expect(
+      {
+        ...updatedTask,
+        labels: updatedTask.labels.map(({ id }) => id),
+      },
+    ).toMatchObject(taskUpdateData);
+  });
+
+  it('tasks#delete', async () => {
+    const res = await server.inject({
+      method: 'DELETE',
+      url: server.reverse('tasks#destroy', { id: 1 }),
+      headers: { cookie },
+    });
+    expect(res.statusCode).toBe(302);
+
+    const deletedTask = await server.objection.models.task.query();
+    expect(deletedTask).toEqual([]);
   });
 });

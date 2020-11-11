06bae44015c227c75cf9a12666d3e64f20902c14
Deputatov Nikita <deputatovn@yandex.ru>
2020-11-11 08:34:32 +0300

    Refactor module labels.js

--- server/routes/labels.js
+++ server/routes/labels.js
@@ -26,7 +26,7 @@ export default (app) => {
           reply.code(err.statusCode).render('labels/new', { label: req.body.object, errors: err.data });
           return reply;
         }
-        reply.code(err.statusCode).type('application/json').send(err.data);
+        reply.send(err);
         return reply;
       }
     })
@@ -34,14 +34,14 @@ export default (app) => {
     .get('/labels/:id/edit', { name: 'labels#edit', preHandler: app.auth([app.verifyAuth]) }, async (req, reply) => {
       try {
         const toEdit = await app.objection.models.label.query().findById(req.params.id);
-        if (toEdit) {
-          reply.render('labels/edit', { label: { ...toEdit } });
+        if (!toEdit) {
+          reply.callNotFound();
           return reply;
         }
-        reply.code(404).type('text/plain').send('Not Found');
+        reply.render('labels/edit', { label: { ...toEdit } });
         return reply;
       } catch (err) {
-        reply.code(err.statusCode).type('application/json').send(err.data);
+        reply.send(err);
         return reply;
       }
     })
@@ -49,13 +49,13 @@ export default (app) => {
     .patch('/labels/:id', { name: 'labels#update', preHandler: app.auth([app.verifyAuth]) }, async (req, reply) => {
       try {
         const toPatch = await app.objection.models.label.query().findById(req.params.id);
-        if (toPatch) {
-          await toPatch.$query().patch(req.body.object);
-          req.flash('info', i18next.t('flash.labels.update.success'));
-          reply.redirect(app.reverse('labels#index'));
+        if (!toPatch) {
+          reply.callNotFound();
           return reply;
         }
-        reply.code(404).type('text/plain').send('Not Found');
+        await toPatch.$query().patch(req.body.object);
+        req.flash('info', i18next.t('flash.labels.update.success'));
+        reply.redirect(app.reverse('labels#index'));
         return reply;
       } catch (err) {
         if (err instanceof ValidationError) {
@@ -69,7 +69,7 @@ export default (app) => {
           });
           return reply;
         }
-        reply.code(err.statusCode).type('application/json').send(err.data);
+        reply.send(err);
         return reply;
       }
     })
@@ -77,16 +77,16 @@ export default (app) => {
     .delete('/labels/:id', { name: 'labels#destroy', preHandler: app.auth([app.verifyAuth]) }, async (req, reply) => {
       try {
         const toDelete = await app.objection.models.label.query().findById(req.params.id);
-        if (toDelete) {
-          await toDelete.$query().delete();
-          req.flash('info', i18next.t('flash.labels.delete.success'));
-          reply.code(204).redirect(302, app.reverse('labels#index'));
+        if (!toDelete) {
+          reply.callNotFound();
           return reply;
         }
-        reply.code(404).type('text/plain').send('Not Found');
+        await toDelete.$query().delete();
+        req.flash('info', i18next.t('flash.labels.delete.success'));
+        reply.code(204).redirect(302, app.reverse('labels#index'));
         return reply;
       } catch (err) {
-        reply.code(err.statusCode).type('application/json').send(err.data);
+        reply.send(err);
         return reply;
       }
     });

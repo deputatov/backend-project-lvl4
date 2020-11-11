9bd58859f87a27f9e422577fb31b4ae4cbcfe93a
Deputatov Nikita <deputatovn@yandex.ru>
2020-11-11 15:47:14 +0300

    Refactor module tasks.js

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -58,7 +58,7 @@ export default (app) => {
         });
         return reply;
       } catch (err) {
-        reply.code(err.statusCode).type('application/json').send(err.data);
+        reply.send(err);
         return reply;
       }
     })
@@ -82,7 +82,7 @@ export default (app) => {
         reply.render('tasks/new', { task });
         return reply;
       } catch (err) {
-        reply.code(err.statusCode).type('application/json').send(err.data);
+        reply.send(err);
         return reply;
       }
     })
@@ -128,7 +128,7 @@ export default (app) => {
           reply.code(err.statusCode).render('tasks/new', { task, errors: err.data });
           return reply;
         }
-        reply.code(err.statusCode).type('application/json').send(err.data);
+        reply.send(err);
         return reply;
       }
     })
@@ -139,14 +139,14 @@ export default (app) => {
           .query()
           .findById(req.params.id)
           .withGraphJoined('[executors, creators, taskStatuses, labels]');
-        if (task) {
-          reply.render('tasks/show', { task });
+        if (!task) {
+          reply.callNotFound();
           return reply;
         }
-        reply.code(404).type('text/plain').send('Not Found');
+        reply.render('tasks/show', { task });
         return reply;
       } catch (err) {
-        reply.code(err.statusCode).type('application/json').send(err.data);
+        reply.send(err);
         return reply;
       }
     })
@@ -160,29 +160,29 @@ export default (app) => {
           .modifyGraph('labels', (builder) => {
             builder.select('labels.id');
           });
-        if (toEdit) {
-          const [
-            allTaskStatuses,
-            allExecutors,
-            allLabels,
-          ] = await Promise.all([
-            app.objection.models.taskStatus.query(),
-            app.objection.models.user.query(),
-            app.objection.models.label.query(),
-          ]);
-          const task = {
-            ...toEdit,
-            taskStatusId: addPropertySelected(allTaskStatuses, toEdit.taskStatusId),
-            executorId: addPropertySelected(allExecutors, toEdit.executorId),
-            labels: addPropertySelected(allLabels, toEdit.labels.map(({ id }) => id)),
-          };
-          reply.render('tasks/edit', { task });
+        if (!toEdit) {
+          reply.callNotFound();
           return reply;
         }
-        reply.code(404).type('text/plain').send('Not Found');
+        const [
+          allTaskStatuses,
+          allExecutors,
+          allLabels,
+        ] = await Promise.all([
+          app.objection.models.taskStatus.query(),
+          app.objection.models.user.query(),
+          app.objection.models.label.query(),
+        ]);
+        const task = {
+          ...toEdit,
+          taskStatusId: addPropertySelected(allTaskStatuses, toEdit.taskStatusId),
+          executorId: addPropertySelected(allExecutors, toEdit.executorId),
+          labels: addPropertySelected(allLabels, toEdit.labels.map(({ id }) => id)),
+        };
+        reply.render('tasks/edit', { task });
         return reply;
       } catch (err) {
-        reply.code(err.statusCode).type('application/json').send(err.data);
+        reply.send(err);
         return reply;
       }
     })
@@ -195,22 +195,22 @@ export default (app) => {
       } = req.body.object;
       try {
         const toPatch = await app.objection.models.task.query().findById(req.params.id);
-        if (toPatch) {
-          await app.objection.models.task.transaction(async (trx) => {
-            await app.objection.models.task
-              .query(trx)
-              .upsertGraph({
-                id: req.params.id,
-                creatorId: req.currentUser.id,
-                ...req.body.object,
-                labels: castArray(selectedLabelsId || []).map((id) => ({ id })),
-              }, { relate: true, unrelate: true });
-          });
-          req.flash('info', i18next.t('flash.tasks.update.success'));
-          reply.code(201).redirect(302, app.reverse('tasks#index'));
+        if (!toPatch) {
+          reply.callNotFound();
           return reply;
         }
-        reply.code(404).type('text/plain').send('Not Found');
+        await app.objection.models.task.transaction(async (trx) => {
+          await app.objection.models.task
+            .query(trx)
+            .upsertGraph({
+              id: req.params.id,
+              creatorId: req.currentUser.id,
+              ...req.body.object,
+              labels: castArray(selectedLabelsId || []).map((id) => ({ id })),
+            }, { relate: true, unrelate: true });
+        });
+        req.flash('info', i18next.t('flash.tasks.update.success'));
+        reply.code(201).redirect(302, app.reverse('tasks#index'));
         return reply;
       } catch (err) {
         if (err instanceof ValidationError) {
@@ -233,7 +233,7 @@ export default (app) => {
           reply.code(err.statusCode).render('tasks/edit', { task, errors: err.data });
           return reply;
         }
-        reply.code(err.statusCode).type('application/json').send(err.data);
+        reply.send(err);
         return reply;
       }
     })
@@ -241,19 +241,19 @@ export default (app) => {
     .delete('/tasks/:id', { name: 'tasks#destroy', preHandler: app.auth([app.verifyAuth, app.asyncVerifyTaskCreator], { relation: 'and' }) }, async (req, reply) => {
       try {
         const toDelete = await app.objection.models.task.query().findById(req.params.id);
-        if (toDelete) {
-          await app.objection.models.task.transaction(async (trx) => {
-            await toDelete.$query(trx).delete();
-            await toDelete.$relatedQuery('labels', trx).unrelate();
-          });
-          req.flash('info', i18next.t('flash.tasks.delete.success'));
-          reply.code(204).redirect(302, app.reverse('tasks#index'));
+        if (!toDelete) {
+          reply.callNotFound();
           return reply;
         }
-        reply.code(404).type('text/plain').send('Not Found');
+        await app.objection.models.task.transaction(async (trx) => {
+          await toDelete.$query(trx).delete();
+          await toDelete.$relatedQuery('labels', trx).unrelate();
+        });
+        req.flash('info', i18next.t('flash.tasks.delete.success'));
+        reply.code(204).redirect(302, app.reverse('tasks#index'));
         return reply;
       } catch (err) {
-        reply.code(err.statusCode).type('application/json').send(err.data);
+        reply.send(err);
         return reply;
       }
     });

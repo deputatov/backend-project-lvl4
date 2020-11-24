de302879783ae6c72352633c63d52e6ed1484b49
Deputatov Nikita <deputatovn@yandex.ru>
2020-11-24 21:54:48 +0300

    Remove unused status code

--- server/lib/auth.js
+++ server/lib/auth.js
@@ -3,7 +3,7 @@ import i18next from 'i18next';
 export const verifyAuth = (app) => (req, reply, done) => {
   if (!req.signedIn) {
     req.flash('error', i18next.t('flash.users.authorizationError'));
-    reply.code(401).redirect(302, app.reverse('root'));
+    reply.redirect(app.reverse('root'));
     return done(new Error('Unauthorized'));
   }
   return done();
@@ -12,7 +12,7 @@ export const verifyAuth = (app) => (req, reply, done) => {
 export const verifyUserCreator = (app) => (req, reply, done) => {
   if (req.currentUser.id !== Number(req.params.id)) {
     req.flash('error', i18next.t('flash.users.accessError'));
-    reply.code(403).redirect(302, app.reverse('users#index'));
+    reply.redirect(app.reverse('users#index'));
     return done(new Error('Forbidden'));
   }
   return done();
@@ -22,7 +22,7 @@ export const asyncVerifyTaskCreator = (app) => async (req, reply) => {
   const task = await app.objection.models.task.query().findById(req.params.id);
   if (task && task.creatorId !== req.currentUser.id) {
     req.flash('error', i18next.t('flash.tasks.accessError'));
-    reply.code(403).redirect(302, app.reverse('tasks#index'));
+    reply.redirect(app.reverse('tasks#index'));
     throw new Error('Forbidden');
   }
 };
